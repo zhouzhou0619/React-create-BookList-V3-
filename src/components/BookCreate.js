ee7a9b921a30ffaa155d61e396327287f01da806
zhouzhou <zhouzzhou0619@gmail.com>
2023-06-02 12:17:14 +1000

    code for creating booklist, a button called 'create!', when user enter the new book's title, and click create!, system will save it.

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,5 +1,23 @@
-function BookCreate() {
-    return <div>BookCreate</div>;
+import { useState } from 'react';
+function BookCreate({ onCreate }) {
+    const [title, setTitle] = useState('');
+    const handleChange = event => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        onCreate(title);
+    };
+    return (
+        <div>
+            <form pnSubmit={handleSubmit}>
+                <label>Title</label>
+                <input value={title} onChange={handleChange} />
+                <button>Create!</button>
+            </form>
+        </div>
+    );
 }
 
 export default BookCreate;
