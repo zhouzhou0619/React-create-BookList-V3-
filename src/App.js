d4b6f637a958ae70b7bf9a4cf3658f4863f172b9
zhouzhou <zhouzzhou0619@gmail.com>
2023-06-06 12:51:20 +1000

    6/06/2023

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import BookCreate from './components/BookCreate';
 function App() {
     const [books, setBooks] = useState([]);
     const createBook = title => {
-        console.log('Need to add book with: ', title);
+        books.push({ id: 1234, title: title });
+        console.log(books);
+        setBooks(books);
     };
     return (
         <div>
+            {books.length}
             <BookCreate onCreate={createBook} />
         </div>
     );
