d979f648b0c6c540c0a234de0b036bd64d902be5
zhouzhou <zhouzzhou0619@gmail.com>
2023-06-07 12:59:41 +1000

    Add function of deleting the books on screen

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 function App() {
     const [books, setBooks] = useState([]);
+    const deleteBookById = id => {
+        const updatedBooks = books.filter(book => {
+            return book.id !== id;
+        });
+
+        setBooks(updatedBooks);
+    };
     const createBook = title => {
         const updatedBooks = [...books, { id: Math.round(Math.random() * 9999), title }];
         setBooks(updatedBooks);
     };
     return (
         <div className="app">
-            <BookList books={books} />
+            <BookList books={books} onDelete={deleteBookById} />
             <BookCreate onCreate={createBook} />
         </div>
     );
