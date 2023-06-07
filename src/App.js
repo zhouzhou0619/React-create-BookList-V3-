3889502e8b3d6559c86c0359030e92fe7c48528d
zhouzhou <zhouzzhou0619@gmail.com>
2023-06-07 12:38:30 +1000

    add the book and show on the screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 function App() {
     const [books, setBooks] = useState([]);
     const createBook = title => {
-        books.push({ id: 1234, title: title });
-        console.log(books);
-        setBooks(books);
+        const updatedBooks = [...books, { id: Math.round(Math.random() * 9999), title }];
+        setBooks(updatedBooks);
     };
     return (
-        <div>
-            {books.length}
+        <div className="app">
+            <BookList books={books} />
             <BookCreate onCreate={createBook} />
         </div>
     );
