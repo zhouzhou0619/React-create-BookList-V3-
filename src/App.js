288bfcf69ed00f0359f10e76406b8ed50335ddc5
zhouzhou <zhouzzhou0619@gmail.com>
2023-06-08 16:46:03 +1000

    When user add/delete/update/check the booklist, the previous changing will still show on the screen, it is  persistent

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 function App() {
     const [books, setBooks] = useState([]);
-    const editBookById = (id, newTitle) => {
+
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+        setBooks(response.data);
+    };
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle,
+        });
+        // console.log(response);
+
         const updateBooks = books.map(book => {
             if (book.id === id) {
-                return { ...book, title: newTitle };
+                return { ...book, ...response.date };
             }
             return book;
         });
         setBooks(updateBooks);
     };
-    const deleteBookById = id => {
+    const deleteBookById = async id => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
         const updatedBooks = books.filter(book => {
             return book.id !== id;
         });
