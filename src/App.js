6e847254915723a2a808a1f2b649c3a1c6b231a5
zhouzhou <zhouzzhou0619@gmail.com>
2023-06-08 13:20:26 +1000

    use axios into function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
@@ -19,8 +20,12 @@ function App() {
 
         setBooks(updatedBooks);
     };
-    const createBook = title => {
-        const updatedBooks = [...books, { id: Math.round(Math.random() * 9999), title }];
+    const createBook = async title => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title,
+        });
+
+        const updatedBooks = [...books, response.data];
         setBooks(updatedBooks);
     };
     return (
