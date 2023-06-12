e85dab472430ddcacd49df7e47d0f512c7e8d4cd
zhouzhou <zhouzzhou0619@gmail.com>
2023-06-12 15:19:50 +1000

    rebuild bookcreate

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,13 +1,16 @@
-import { useState } from 'react';
-function BookCreate({ onCreate }) {
+import { useState, useContext } from 'react';
+import BooksContext from '../context/books';
+
+function BookCreate() {
     const [title, setTitle] = useState('');
+    const { createBook } = useContext(BooksContext);
     const handleChange = event => {
         setTitle(event.target.value);
     };
 
     const handleSubmit = event => {
         event.preventDefault();
-        onCreate(title);
+        createBook(title);
         setTitle('');
     };
     return (
