06096e733e55beeea8295c8452d97e05e3d04fdb
zhouzhou <zhouzzhou0619@gmail.com>
2023-06-12 14:58:24 +1000

    Reconfiguration process Places content that originally existed in the App into the Provider for easy use { useContext }.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,9 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 function BookList({ books, onDelete, onEdit }) {
+    const { count, incrementCount } = useContext(BooksContext);
+
     const renderedBooks = books.map(book => {
         return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />;
     });
